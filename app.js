d6fda15ee4eab65c3f7ff181aa2346ed23c38763
Tomek Rybczyński <tomek.rybka@gmail.com>
2016-10-01 17:01:49 +0200

    Run on port provided by heroku

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app     = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 
 app.get('/operacja-drawsko', function (req, res) {
   res.sendFile('./view/presentation.html', { root: __dirname });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
